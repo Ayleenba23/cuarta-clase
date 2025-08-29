0638fb8675d3b70c40ecf823581ebe02a075e33b
Ayleenba23 <ayleenbahamondezlatorre@gmail.com>
2025-08-29 13:22:14 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var x = 0;
+var y = 0;
+var canvas; 
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("cuerpo");
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+frameRate (12);
+  noStroke();
+ 
+}
+
+function draw() {
+  colorMode(HSB,359,100,100)
+  background(map(mouseX, 0, windowWidth,0, 360),100,map(mouseY, 0, windowHeight,50,100));
+  x = lerp(x, mouseX, 0.1);
+  y = lerp(y, mouseY, 0.1);
+  fill("white");
+  textSize(random (100,200));
+  text("🫶",x,y);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
